bc0051f18e953bb7f27cc0d98716dec79a178763
Josh Marinacci <joshua@marinacci.org>
2020-04-23 13:52:40 -0700

    update notes

--- breakout.js
+++ breakout.js
@@ -1,3 +1,10 @@
+// josh@josh.earth adapted this tutorial from
+// https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Create_the_Canvas_and_draw_on_it
+
+
+
+
+
 import {Component, System, World} from "./node_modules/ecsy/build/ecsy.module.js"
 import {startWorld} from './ecsytwo.js'
 
