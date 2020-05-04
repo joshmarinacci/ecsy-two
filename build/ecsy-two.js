da2f22b726847c8bdab73b425293d8c5b8747b29
josh <joshua@marinacci.org>
2020-05-04 15:24:55 -0700

    update build

--- build/ecsy-two.js
+++ build/ecsy-two.js
@@ -732,6 +732,8 @@
 	        this.y = 0;
 	        this.width = 10;
 	        this.height = 10;
+	        this.layer = "default";
+	        this.draw_object = null;
 	    }
 	    left() {
 	        return this.x
@@ -839,6 +841,146 @@
 	    }
 	}
 
+	class Layer extends Component{
+	    constructor() {
+	        super();
+	        this.name = "unnamed-layer";
+	        this.depth = 100000;
+	    }
+	}
+	class LayerParent extends Component {
+	    constructor() {
+	        super();
+	        this.name = "default";
+	        this.draw_object = null;
+	    }
+	}
+
+
+	class LayerRenderingSystem extends System {
+	    init() {
+	        this.layer_order = [];
+	        this.layer_index = {};
+	    }
+	    execute(delta, time) {
+	        //this creates a default layer for anything that doesn't specify a layer
+	        this.queries.canvas.added.forEach(ent => {
+	            ent.addComponent(Layer, { name:'default', depth: 0});
+	        });
+	        this.update_layer_order();
+	        this.update_layer_index();
+	        this.draw_layers();
+	    }
+
+	    update_layer_order() {
+	        this.layer_order = this.queries.layers.results.map(ent => ent.getComponent(Layer));
+	        this.layer_order.sort((a,b)=> a.depth - b.depth);
+	    }
+
+	    update_layer_index() {
+	        this.layer_index = {};
+	        this.queries.layer_children.results.forEach(ent => {
+	            let ch = ent.getComponent(LayerParent);
+	            if(!this.layer_index[ch.name]) this.layer_index[ch.name] = [];
+	            if(ch.draw_object) this.layer_index[ch.name].push(ch.draw_object);
+	        });
+	        this.queries.sprite_children.results.forEach(ent => {
+	            let ch = ent.getComponent(Sprite);
+	            if(!this.layer_index[ch.layer]) this.layer_index[ch.layer] = [];
+	            if(ch.draw_object) this.layer_index[ch.layer].push(ch.draw_object);
+	        });
+	    }
+
+	    draw_layers() {
+	        this.queries.canvas.results.forEach(canvas_ent => {
+	            let canvas = canvas_ent.getComponent(Canvas);
+	            let ctx = canvas.dom.getContext('2d');
+	            ctx.save();
+	            ctx.scale(canvas.scale,canvas.scale);
+	            if(canvas_ent.hasComponent(Camera)) {
+	                let camera = canvas_ent.getComponent(Camera);
+	                if (camera.centered) {
+	                    ctx.translate(
+	                        -camera.x + canvas.width / 2,
+	                        -camera.y + canvas.height / 2);
+	                }
+	            }
+	            this.layer_order.forEach(layer => {
+	                let children = this.layer_index[layer.name];
+	                if(children) children.forEach(ch => ch.draw(ctx, canvas_ent));
+	            });
+	            ctx.restore();
+	        });
+	    }
+	}
+	LayerRenderingSystem.queries = {
+	    canvas: {
+	        components: [Canvas],
+	        listen: {
+	            added:true
+	        }
+	    },
+	    layers: {
+	        components:[Layer]
+	    },
+	    layer_children: {
+	        components: [LayerParent]
+	    },
+	    sprite_children: {
+	        components: [Sprite]
+	    },
+	};
+
+
+	class DrawFilledRect {
+	    constructor(bounds,color) {
+	        this.bounds = bounds;
+	        this.color = color;
+	    }
+	    draw(ctx) {
+	        ctx.fillStyle = this.color;
+	        ctx.fillRect(this.bounds.x,this.bounds.y,this.bounds.width,this.bounds.height);
+	    }
+	}
+
+	class DrawStrokedRect {
+	    constructor(bounds,color) {
+	        this.bounds = bounds;
+	        this.color = color;
+	    }
+	    draw(ctx) {
+	        ctx.strokeStyle = this.color;
+	        ctx.strokeRect(this.bounds.x,this.bounds.y,this.bounds.width,this.bounds.height);
+	    }
+	}
+
+	class DrawImage {
+	    constructor(bounds, image_sprite) {
+	        this.bounds = bounds;
+	        this.sprite = image_sprite;
+	    }
+	    draw(ctx, ent) {
+	        if(this.sprite && this.sprite.image) {
+	            ctx.save();
+	            if(this.sprite.flipY) {
+	                ctx.scale(-1,1);
+	                ctx.translate(-this.sprite.width,0);
+	            }
+	            if(this.bounds.fixed && ent.hasComponent(Camera)) {
+	                let canvas = ent.getComponent(Canvas);
+	                let camera = ent.getComponent(Camera);
+	                ctx.translate(
+	                    +camera.x - canvas.width/2,
+	                    +camera.y - canvas.height/2);
+	            }
+	            ctx.translate(this.bounds.x,this.bounds.y);
+	            ctx.drawImage(this.sprite.image, 0, 0);
+	            ctx.restore();
+	        }
+
+	    }
+	}
+
 	class ImageSprite extends Component {
 	    constructor() {
 	        super();
@@ -899,29 +1041,21 @@
 	            this.queries.filled_sprites.results.forEach(ent => {
 	                let sprite = ent.getComponent(Sprite);
 	                let color = ent.getComponent(FilledSprite);
-	                ctx.fillStyle = color.color;
-	                ctx.fillRect(sprite.x, sprite.y, sprite.width, sprite.height);
+	                sprite.draw_object = new DrawFilledRect(sprite,color.color);
 	            });
 	            // draw image sprites
 	            this.queries.sprites.results.forEach(ent => {
 	                let sprite = ent.getComponent(Sprite);
-	                ctx.save();
+	                /*
 	                if(sprite.fixed && canvas_ent.hasComponent(Camera)) {
-	                    let camera = canvas_ent.getComponent(Camera);
+	                    let camera = canvas_ent.getComponent(Camera)
 	                    ctx.translate(
 	                        +camera.x - canvas.width/2,
-	                        +camera.y - canvas.height/2);
+	                        +camera.y - canvas.height/2)
 	                }
-	                ctx.translate(sprite.x, sprite.y);
+	                 */
 	                let image_sprite = ent.getComponent(ImageSprite);
-	                if(image_sprite.flipY) {
-	                    ctx.scale(-1,1);
-	                    ctx.translate(-sprite.width,0);
-	                }
-	                if(image_sprite.image) {
-	                    ctx.drawImage(image_sprite.image, 0, 0);
-	                }
-	                ctx.restore();
+	                sprite.draw_object = new DrawImage(sprite,image_sprite);
 	            });
 
 	            // draw animated images sprites
@@ -950,6 +1084,7 @@
 	            this.queries.debug_sprites.results.forEach(ent => {
 	                let sprite = ent.getComponent(Sprite);
 	                let debug = ent.getComponent(DebugOutline);
+	                sprite.draw_object = new DrawStrokedRect(sprite,debug.color);
 	                ctx.strokeStyle = debug.color;
 	                ctx.strokeRect(sprite.x, sprite.y, sprite.width, sprite.height);
 	            });
@@ -1098,23 +1233,19 @@
 	    execute(delta, time) {
 	        this.queries.maps.results.forEach(ent => {
 	            let map = ent.getComponent(TileMap);
-	            this.queries.screen.results.forEach(ent => {
-	                let canvas = ent.getComponent(Canvas);
-	                let camera = ent.getComponent(Camera);
-	                let ctx = canvas.dom.getContext('2d');
-	                ctx.imageSmoothingEnabled = !canvas.pixelMode;
-	                ctx.save();
-	                ctx.scale(canvas.scale,canvas.scale);
-	                ctx.translate(
-	                    -camera.x + canvas.width/2,
-	                    -camera.y + canvas.height/2
-	                );
-	                map.layers.forEach(layer => {
-	                    if(layer.type === 'tilelayer') this.drawTileLayer(map, ctx, layer);
-	                    if(layer.type === 'objectgroup') this.drawObjectLayer(map, ctx, layer);
-	                });
-	                ctx.restore();
-	            });
+	            let layer = ent.getComponent(LayerParent);
+	            layer.draw_object = {
+	                draw:(ctx)=>{
+	                    this.drawMap(map,ctx);
+	                }
+	            };
+	        });
+	    }
+
+	    drawMap(map,ctx) {
+	        map.layers.forEach(layer => {
+	            if (layer.type === 'tilelayer') this.drawTileLayer(map, ctx, layer);
+	            if (layer.type === 'objectgroup') this.drawObjectLayer(map, ctx, layer);
 	        });
 	    }
 
@@ -1159,7 +1290,7 @@
 	        components:[Canvas, Camera]
 	    },
 	    maps: {
-	        components:[TileMap],
+	        components:[TileMap, LayerParent],
 	    }
 	};
 
@@ -1756,6 +1887,9 @@
 	exports.DebugOutline = DebugOutline;
 	exports.Dialog = Dialog;
 	exports.DialogSystem = DialogSystem;
+	exports.DrawFilledRect = DrawFilledRect;
+	exports.DrawImage = DrawImage;
+	exports.DrawStrokedRect = DrawStrokedRect;
 	exports.ECSYTwoSystem = ECSYTwoSystem;
 	exports.FilledSprite = FilledSprite;
 	exports.FullscreenButton = FullscreenButton;
@@ -1765,6 +1899,9 @@
 	exports.InputState = InputState;
 	exports.KeyboardState = KeyboardState;
 	exports.KeyboardSystem = KeyboardSystem;
+	exports.Layer = Layer;
+	exports.LayerParent = LayerParent;
+	exports.LayerRenderingSystem = LayerRenderingSystem;
 	exports.MouseInputSystem = MouseInputSystem;
 	exports.MouseState = MouseState;
 	exports.PlaySoundEffect = PlaySoundEffect;
--- build/ecsy-two.module.js
+++ build/ecsy-two.module.js
@@ -721,6 +721,8 @@ class Sprite extends  Component {
         this.y = 0;
         this.width = 10;
         this.height = 10;
+        this.layer = "default";
+        this.draw_object = null;
     }
     left() {
         return this.x
@@ -828,6 +830,146 @@ class InputState extends Component {
     }
 }
 
+class Layer extends Component{
+    constructor() {
+        super();
+        this.name = "unnamed-layer";
+        this.depth = 100000;
+    }
+}
+class LayerParent extends Component {
+    constructor() {
+        super();
+        this.name = "default";
+        this.draw_object = null;
+    }
+}
+
+
+class LayerRenderingSystem extends System {
+    init() {
+        this.layer_order = [];
+        this.layer_index = {};
+    }
+    execute(delta, time) {
+        //this creates a default layer for anything that doesn't specify a layer
+        this.queries.canvas.added.forEach(ent => {
+            ent.addComponent(Layer, { name:'default', depth: 0});
+        });
+        this.update_layer_order();
+        this.update_layer_index();
+        this.draw_layers();
+    }
+
+    update_layer_order() {
+        this.layer_order = this.queries.layers.results.map(ent => ent.getComponent(Layer));
+        this.layer_order.sort((a,b)=> a.depth - b.depth);
+    }
+
+    update_layer_index() {
+        this.layer_index = {};
+        this.queries.layer_children.results.forEach(ent => {
+            let ch = ent.getComponent(LayerParent);
+            if(!this.layer_index[ch.name]) this.layer_index[ch.name] = [];
+            if(ch.draw_object) this.layer_index[ch.name].push(ch.draw_object);
+        });
+        this.queries.sprite_children.results.forEach(ent => {
+            let ch = ent.getComponent(Sprite);
+            if(!this.layer_index[ch.layer]) this.layer_index[ch.layer] = [];
+            if(ch.draw_object) this.layer_index[ch.layer].push(ch.draw_object);
+        });
+    }
+
+    draw_layers() {
+        this.queries.canvas.results.forEach(canvas_ent => {
+            let canvas = canvas_ent.getComponent(Canvas);
+            let ctx = canvas.dom.getContext('2d');
+            ctx.save();
+            ctx.scale(canvas.scale,canvas.scale);
+            if(canvas_ent.hasComponent(Camera)) {
+                let camera = canvas_ent.getComponent(Camera);
+                if (camera.centered) {
+                    ctx.translate(
+                        -camera.x + canvas.width / 2,
+                        -camera.y + canvas.height / 2);
+                }
+            }
+            this.layer_order.forEach(layer => {
+                let children = this.layer_index[layer.name];
+                if(children) children.forEach(ch => ch.draw(ctx, canvas_ent));
+            });
+            ctx.restore();
+        });
+    }
+}
+LayerRenderingSystem.queries = {
+    canvas: {
+        components: [Canvas],
+        listen: {
+            added:true
+        }
+    },
+    layers: {
+        components:[Layer]
+    },
+    layer_children: {
+        components: [LayerParent]
+    },
+    sprite_children: {
+        components: [Sprite]
+    },
+};
+
+
+class DrawFilledRect {
+    constructor(bounds,color) {
+        this.bounds = bounds;
+        this.color = color;
+    }
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.bounds.x,this.bounds.y,this.bounds.width,this.bounds.height);
+    }
+}
+
+class DrawStrokedRect {
+    constructor(bounds,color) {
+        this.bounds = bounds;
+        this.color = color;
+    }
+    draw(ctx) {
+        ctx.strokeStyle = this.color;
+        ctx.strokeRect(this.bounds.x,this.bounds.y,this.bounds.width,this.bounds.height);
+    }
+}
+
+class DrawImage {
+    constructor(bounds, image_sprite) {
+        this.bounds = bounds;
+        this.sprite = image_sprite;
+    }
+    draw(ctx, ent) {
+        if(this.sprite && this.sprite.image) {
+            ctx.save();
+            if(this.sprite.flipY) {
+                ctx.scale(-1,1);
+                ctx.translate(-this.sprite.width,0);
+            }
+            if(this.bounds.fixed && ent.hasComponent(Camera)) {
+                let canvas = ent.getComponent(Canvas);
+                let camera = ent.getComponent(Camera);
+                ctx.translate(
+                    +camera.x - canvas.width/2,
+                    +camera.y - canvas.height/2);
+            }
+            ctx.translate(this.bounds.x,this.bounds.y);
+            ctx.drawImage(this.sprite.image, 0, 0);
+            ctx.restore();
+        }
+
+    }
+}
+
 class ImageSprite extends Component {
     constructor() {
         super();
@@ -888,29 +1030,21 @@ class SpriteSystem extends System {
             this.queries.filled_sprites.results.forEach(ent => {
                 let sprite = ent.getComponent(Sprite);
                 let color = ent.getComponent(FilledSprite);
-                ctx.fillStyle = color.color;
-                ctx.fillRect(sprite.x, sprite.y, sprite.width, sprite.height);
+                sprite.draw_object = new DrawFilledRect(sprite,color.color);
             });
             // draw image sprites
             this.queries.sprites.results.forEach(ent => {
                 let sprite = ent.getComponent(Sprite);
-                ctx.save();
+                /*
                 if(sprite.fixed && canvas_ent.hasComponent(Camera)) {
-                    let camera = canvas_ent.getComponent(Camera);
+                    let camera = canvas_ent.getComponent(Camera)
                     ctx.translate(
                         +camera.x - canvas.width/2,
-                        +camera.y - canvas.height/2);
+                        +camera.y - canvas.height/2)
                 }
-                ctx.translate(sprite.x, sprite.y);
+                 */
                 let image_sprite = ent.getComponent(ImageSprite);
-                if(image_sprite.flipY) {
-                    ctx.scale(-1,1);
-                    ctx.translate(-sprite.width,0);
-                }
-                if(image_sprite.image) {
-                    ctx.drawImage(image_sprite.image, 0, 0);
-                }
-                ctx.restore();
+                sprite.draw_object = new DrawImage(sprite,image_sprite);
             });
 
             // draw animated images sprites
@@ -939,6 +1073,7 @@ class SpriteSystem extends System {
             this.queries.debug_sprites.results.forEach(ent => {
                 let sprite = ent.getComponent(Sprite);
                 let debug = ent.getComponent(DebugOutline);
+                sprite.draw_object = new DrawStrokedRect(sprite,debug.color);
                 ctx.strokeStyle = debug.color;
                 ctx.strokeRect(sprite.x, sprite.y, sprite.width, sprite.height);
             });
@@ -1087,23 +1222,19 @@ class TileMapSystem extends System {
     execute(delta, time) {
         this.queries.maps.results.forEach(ent => {
             let map = ent.getComponent(TileMap);
-            this.queries.screen.results.forEach(ent => {
-                let canvas = ent.getComponent(Canvas);
-                let camera = ent.getComponent(Camera);
-                let ctx = canvas.dom.getContext('2d');
-                ctx.imageSmoothingEnabled = !canvas.pixelMode;
-                ctx.save();
-                ctx.scale(canvas.scale,canvas.scale);
-                ctx.translate(
-                    -camera.x + canvas.width/2,
-                    -camera.y + canvas.height/2
-                );
-                map.layers.forEach(layer => {
-                    if(layer.type === 'tilelayer') this.drawTileLayer(map, ctx, layer);
-                    if(layer.type === 'objectgroup') this.drawObjectLayer(map, ctx, layer);
-                });
-                ctx.restore();
-            });
+            let layer = ent.getComponent(LayerParent);
+            layer.draw_object = {
+                draw:(ctx)=>{
+                    this.drawMap(map,ctx);
+                }
+            };
+        });
+    }
+
+    drawMap(map,ctx) {
+        map.layers.forEach(layer => {
+            if (layer.type === 'tilelayer') this.drawTileLayer(map, ctx, layer);
+            if (layer.type === 'objectgroup') this.drawObjectLayer(map, ctx, layer);
         });
     }
 
@@ -1148,7 +1279,7 @@ TileMapSystem.queries = {
         components:[Canvas, Camera]
     },
     maps: {
-        components:[TileMap],
+        components:[TileMap, LayerParent],
     }
 };
 
@@ -1736,4 +1867,4 @@ AudioSystem.queries = {
     }
 };
 
-export { AnimatedSprite, AudioSystem, BackgroundFill, Camera, CameraFollowsSprite, Canvas, DebugOutline, Dialog, DialogSystem, ECSYTwoSystem, FilledSprite, FullscreenButton, FullscreenMode, FullscreenSystem, ImageSprite, InputState, KeyboardState, KeyboardSystem, MouseInputSystem, MouseState, PlaySoundEffect, SoundEffect, Sprite, SpriteSheet, SpriteSystem, TileMap, TileMapSystem, WaitForInput, load_image_from_url, load_tilemap_from_url, make_point, startWorld };
+export { AnimatedSprite, AudioSystem, BackgroundFill, Camera, CameraFollowsSprite, Canvas, DebugOutline, Dialog, DialogSystem, DrawFilledRect, DrawImage, DrawStrokedRect, ECSYTwoSystem, FilledSprite, FullscreenButton, FullscreenMode, FullscreenSystem, ImageSprite, InputState, KeyboardState, KeyboardSystem, Layer, LayerParent, LayerRenderingSystem, MouseInputSystem, MouseState, PlaySoundEffect, SoundEffect, Sprite, SpriteSheet, SpriteSystem, TileMap, TileMapSystem, WaitForInput, load_image_from_url, load_tilemap_from_url, make_point, startWorld };
